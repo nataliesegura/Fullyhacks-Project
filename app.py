98f302e2808376e0b4a29e2ac08a667bb3ffb710
Austin76-cs <poptart4000@gmail.com>
2025-04-12 18:52:00 -0700

    folders

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def landingpage():
+    return render_template('landingpage.html')
+
+@app.route("/Register")
+def Register():
+    return render_template('home.html')
+
+@app.route("/Login")
+def Login():
+    return render_template('home.html')
+
+@app.route("/Contacts")
+def Contacts():
+    return render_template('home.html')
+
+@app.route("/Results")
+def Results():
+    return render_template('home.html')
+
+if __name__ == 'main':
+    app.run(debug=True)
+
